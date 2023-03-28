1a6b919966ca0e50d0250bd30dbf9014a81ecce5
Shahnawaz <shahnawaz132007@gmail.com>
2023-03-28 08:39:51 +0530

    resolved some routing problems

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#/">
+          <Link className="navbar-brand" to="/">
             TextUtils
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -23,14 +23,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/about">
                   About
-                </a>
+                </Link>
               </li>
             </ul>
             <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
@@ -49,5 +49,5 @@ export default function Navbar(props) {
         </div>
       </nav>
     </>
-  )
+  );
 }
