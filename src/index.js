ced871760544d801b4507e4bcd161be1feb1bbd7
Shahnawaz <shahnawaz132007@gmail.com>
2023-03-28 09:29:29 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createVercelAnalytics } from '@vercel/analytics';
-
-const analytics = createVercelAnalytics({
-  projectId: 'prj_mqn2uSOjlkzXGebiRPKqHRDPsI3Z',
-  url: 'https://textutils-lovat.vercel.app/'
-});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals(analytics.report);
+reportWebVitals();
