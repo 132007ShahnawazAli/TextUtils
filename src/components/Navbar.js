974a74773186688b9d0c1a16898647e28e47282f
Shahnawaz <shahnawaz132007@gmail.com>
2023-04-03 15:59:28 +0530

    Very minor change in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,14 +23,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <NavLink exact className="nav-link" activeClassName="active" to="/">
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <NavLink className="nav-link" activeClassName="active" to="/about">
                   About
-                </Link>
+                </NavLink>
               </li>
             </ul>
             <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
